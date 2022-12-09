7ff7810e8ad4edc4bc0863641103718ef13709d3
KumarB95 <kumarbalingal@gmail.com>
2022-12-10 02:28:14 +0530

    Qrcode customization

--- qr_code.py
+++ qr_code.py
@@ -1,3 +1,10 @@
-import qrcode as qr
-img = qr.make("Navalkumar Balingal")
-img.save("my_name.png")
+import qrcode
+from PIL import Image
+
+qr = qrcode.QRCode(version=1,
+                   error_correction=qrcode.constants.ERROR_CORRECT_H,
+                   box_size=10, border=4)
+qr.add_data("https://www.linkedin.com/in/kumarbalingal/")
+qr.make(fit=True)
+img = qr.make_image(fill_color="red", back_color="white")
+img.save("new_qr_code.png")
